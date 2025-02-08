b4751a1051fcee4e5d13f638390c27274d40c9ec
WailBenoulha <wailbnlh@gmail.com>
2025-02-08 04:13:01 -1100

    allow hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-170-217-72.eu-west-2.compute.amazonaws.com','127.0.0.1']
 
 
 # Application definition
